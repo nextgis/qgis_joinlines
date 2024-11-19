506009bb9a9dbac74962666d488c31b2b9a5a67a
Ivan Barsukov <i.barsukov@protonmail.com>
2024-11-19 21:13:47 +0700

    Update translation

--- src/joinlines/i18n/joinlines_ru.ts
+++ src/joinlines/i18n/joinlines_ru.ts
@@ -4,68 +4,112 @@
 <context>
     <name>AboutDialog</name>
     <message>
-        <location filename="../about_dialog.py" line="83"/>
+        <location filename="../about_dialog.py" line="206"/>
         <source>Developers</source>
         <translation>Разработчики</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="84"/>
+        <location filename="../about_dialog.py" line="207"/>
         <source>Homepage</source>
         <translation>Домашняя страница</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="85"/>
+        <location filename="../about_dialog.py" line="208"/>
         <source>Please report bugs at</source>
         <translation>Пожалуйста, сообщайте о багах в</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="88"/>
+        <location filename="../about_dialog.py" line="211"/>
         <source>Video with an overview of the plugin</source>
         <translation>Видео с обзором на плагин</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="89"/>
+        <location filename="../about_dialog.py" line="212"/>
         <source>Other helpful services by NextGIS</source>
         <translation>Другие полезные сервисы от NextGIS</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="90"/>
+        <location filename="../about_dialog.py" line="213"/>
         <source>Convenient up-to-date data extracts for any place in the world</source>
         <translation>Удобная выборка актуальных данных из любой точки мира</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="93"/>
+        <location filename="../about_dialog.py" line="216"/>
         <source>Fully featured Web GIS service</source>
         <translation>Полнофункциональный Веб ГИС-сервис</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="87"/>
+        <location filename="../about_dialog.py" line="210"/>
         <source>bugtracker</source>
         <translation>багтрекер</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="78"/>
+        <location filename="../about_dialog.py" line="201"/>
         <source>REPORT_END</source>
         <translation></translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="61"/>
+        <source> by NextGIS</source>
+        <translation> от NextGIS</translation>
     </message>
 </context>
 <context>
     <name>AboutDialogBase</name>
     <message>
-        <location filename="../about_dialog_base.ui" line="14"/>
+        <location filename="../ui/about_dialog_base.ui" line="14"/>
         <source>About {plugin_name}</source>
-        <translation>О {plugin_name}</translation>
+        <translation>О модуле {plugin_name}</translation>
     </message>
     <message>
-        <location filename="../about_dialog_base.ui" line="27"/>
-        <source>&lt;p align=&quot;center&quot;&gt;{plugin_name}&lt;/p&gt;</source>
-        <translation>&lt;p align=&quot;center&quot;&gt;{plugin_name}&lt;/p&gt;</translation>
+        <location filename="../ui/about_dialog_base.ui" line="33"/>
+        <source>Information</source>
+        <translation>Информация</translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="54"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="72"/>
+        <source>License</source>
+        <translation>Лицензия</translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="94"/>
+        <source>Components</source>
+        <translation>Компоненты</translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="116"/>
+        <source>Contributors</source>
+        <translation>Участники</translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="158"/>
+        <source>{plugin_name}</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="168"/>
+        <source>Version {version}</source>
+        <translation>Версия {version}</translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="203"/>
+        <source>Get involved</source>
+        <translation>Присоединяйтесь</translation>
     </message>
 </context>
 <context>
     <name>joinlines</name>
     <message>
-        <location filename="../joinlines.py" line="63"/>
+        <location filename="../joinlines.py" line="72"/>
         <source>&amp;Join two lines</source>
         <translation>&amp;Объединить две линии</translation>
     </message>
@@ -90,52 +134,52 @@
         <translation>О модуле</translation>
     </message>
     <message>
-        <location filename="../joinlines.py" line="96"/>
+        <location filename="../joinlines.py" line="105"/>
         <source>No layers selected</source>
         <translation>Не выбраны слои</translation>
     </message>
     <message>
-        <location filename="../joinlines.py" line="116"/>
+        <location filename="../joinlines.py" line="125"/>
         <source>Warning</source>
         <translation>Предупреждение</translation>
     </message>
     <message>
-        <location filename="../joinlines.py" line="102"/>
+        <location filename="../joinlines.py" line="111"/>
         <source>Not a vector layer</source>
         <translation>Слой не векторный</translation>
     </message>
     <message>
-        <location filename="../joinlines.py" line="108"/>
+        <location filename="../joinlines.py" line="117"/>
         <source>Not a line layer</source>
         <translation>Геометрийя слоя не линейная</translation>
     </message>
     <message>
-        <location filename="../joinlines.py" line="115"/>
+        <location filename="../joinlines.py" line="124"/>
         <source>Only two lines should be selected</source>
         <translation>Должны быть выбраны только две линии</translation>
     </message>
     <message>
-        <location filename="../joinlines.py" line="133"/>
+        <location filename="../joinlines.py" line="142"/>
         <source>Intersection contains more then 1 point</source>
         <translation>Пересечение содержит больше одной точки</translation>
     </message>
     <message>
-        <location filename="../joinlines.py" line="183"/>
+        <location filename="../joinlines.py" line="192"/>
         <source>Join error</source>
         <translation>Ошибка объединения</translation>
     </message>
     <message>
-        <location filename="../joinlines.py" line="183"/>
+        <location filename="../joinlines.py" line="192"/>
         <source>Lines should have common point or intersection</source>
         <translation>Линии должны иметь общую точку, либо пересечение</translation>
     </message>
     <message>
-        <location filename="../joinlines.py" line="258"/>
+        <location filename="../joinlines.py" line="267"/>
         <source>Join selected lines</source>
         <translation>Объединение выделенных линий</translation>
     </message>
     <message>
-        <location filename="../joinlines.py" line="261"/>
+        <location filename="../joinlines.py" line="270"/>
         <source>Delete feature</source>
         <translation>Удаление объекта</translation>
     </message>
